52e718a13bc743be258816eda874f6e7982ed3fb
Nour Harrak <n01154202@humber.ca>
2025-03-13 22:13:28 -0400

    Added form validation for contact section using javascript

--- script.js
+++ script.js
@@ -29,3 +29,57 @@ document.querySelectorAll(".carousel").forEach(carousel => {
     buttons[0].classList.add("carousel_button--selected");
     subtitles[0].classList.add("carousel_subtitle--selected");
 });
+
+//Form Validation
+const form = document.getElementById(`contactForm`);
+
+const nameInput = document.getElementById('name');
+const emailInput = document.getElementById('email');
+const messageInput = document.getElementById('message');
+
+const nameError = document.getElementById('nameError');
+const emailError = document.getElementById('emailError');
+const messageError = document.getElementById('messageError');
+
+const successMessage = document.getElementById('successMessage');
+
+form.addEventListener('submit', function(event){
+nameError.textContent = '';
+emailError.textContent = '';
+messageError.textContent = '';
+successMessage.textContent = '';
+
+let isValid = true;
+
+if (nameInput.value.trim() === '') {
+    nameError.textContent = 'Please Enter Your Name';
+    event.preventDefault();
+    isValid = false;
+};
+
+if (emailInput.value.trim() === '') {
+    emailError.textContent = 'Please Enter Your Email';
+    event.preventDefault();
+    isValid = false;
+} else if(!validateEmail(emailInput.value)) {
+    emailError.textContent = "This Email Address is Not Valid"
+    event.preventDefault();
+    isValid = false;
+}
+
+if (messageInput.value.trim() === '') {
+    messageError.textContent = 'Your Message is Empty!'
+    event.preventDefault();
+    isValid = false;
+}
+
+if(isValid) {
+    successMessage.textContent = "Thanks for reaching out! I'll get back to you as soon as I can!"
+}
+});
+
+//Email Validation
+function validateEmail(email){
+    const re = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    return re.test(String(email));
+}
